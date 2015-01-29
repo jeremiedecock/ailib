d61a66e43ea5c838e38640dd719e1a3e790958c9
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-01-29 22:48:55 +0100

    Remove Python 2.x .pyc files.

--- optimization_framework/clean.sh
+++ optimization_framework/clean.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-#find src -type d -name "__pycache__" -exec rm -rv {} \;
-#find src -type f -name "*.pyc" -exec rm -v {} \;
+# Python 3.x
 
+#find src -type d -name "__pycache__" -exec rm -rv {} \;
 rm -rvf function/__pycache__
 rm -rvf optimizer/__pycache__
+
+# Python 2.x
+
+find . -type f -name "*.pyc" -exec rm -v {} \;
