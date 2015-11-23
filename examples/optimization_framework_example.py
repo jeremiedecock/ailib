c07824a3f725723a8b3ae6b53376cc0e1cb67ccd
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-11-23 07:10:26 +0100

    Move the optimization example script to 'examples/'.

--- examples/optimization_framework_example.py
+++ examples/optimization_framework_example.py
@@ -21,54 +21,62 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import numpy as np
+"""
+A PyAI (optimization framework) demo.
+"""
 import math
+import numpy as np
 
+# CONFIG ######################################################################
+
+objective_func = "sphere"
+optimizer_choice = "saes"
 
 # MAIN ########################################################################
 
 def main():
+    """
+    A PyAI (optimization framework) demo.
+    """
 
     # SETUP OBJECTIVE FUNCTION ############################
 
-    objective_func = "sphere"
-
     if objective_func == "sphere":
         # Sphere ##########################
-        from function.sphere import Function
+        from pyai.optimization.function.sphere import Function
         #f = Function(1)
         f = Function(2)
         #f = Function(10)
 
     elif objective_func == "noised_sphere":
         # Noised sphere ###################
-        from function.noised_sphere import Function
+        from pyai.optimization.function.noised_sphere import Function
         #f = Function(1)
         f = Function(2)
 
     elif objective_func == "sin1":
         # Sinusoid functions ##############
-        from function.sin1 import Function
+        from pyai.optimization.function.sin1 import Function
         f = Function()
 
     elif objective_func == "sin2":
         # Sinusoid functions ##############
-        from function.sin2 import Function
+        from pyai.optimization.function.sin2 import Function
         f = Function()
 
     elif objective_func == "sin3":
         # Sinusoid functions ##############
-        from function.sin3 import Function
+        from pyai.optimization.function.sin3 import Function
         f = Function()
 
     elif objective_func == "yahoo":
         # Yahoo function ##################
-        from function.yahoo import Function
+        from pyai.optimization.function.yahoo import Function
         f = Function()
 
     elif objective_func == "deg_2_poly":
         # Degree 2 polynomial function ####
-        from function.degree_2_polynomial import Function
+        from pyai.optimization.function.degree_2_polynomial import Function
         f = Function(np.array([6.,2.]), np.array([1.,2.]), 1., 2)
 
     else:
@@ -80,31 +88,29 @@ def main():
 
     # OPTIMIZER ###########################################
 
-    optimizer_choice = "cutting_plane"
-
     if optimizer_choice == "naive":
         # Naive Minimizer #################
-        from optimizer.naive import Optimizer
+        from pyai.optimization.optimizer.naive import Optimizer
         optimizer = Optimizer()
         best_x = optimizer.optimize(f, num_samples=300)
 
     elif optimizer_choice == "gradient":
         # Gradient descent ################
-        from optimizer.gradient import Optimizer
+        from pyai.optimization.optimizer.gradient import Optimizer
         optimizer = Optimizer()
         f.delta = 0.01
         best_x = optimizer.optimize(f, num_iterations=30)
 
     elif optimizer_choice == "saes":
         # SAES ############################
-        from optimizer.saes_hgb import Optimizer
+        from pyai.optimization.optimizer.saes_hgb import Optimizer
         optimizer = Optimizer(x_init=np.ones(f.ndim), num_evals_func=lambda gen_index: math.floor(10. * pow(gen_index, 0.5)))
         optimizer = Optimizer(x_init=np.ones(f.ndim))
         best_x = optimizer.optimize(f, num_gen=50)
 
     elif optimizer_choice == "cutting_plane":
         # Cutting plane ###################
-        from optimizer.cutting_plane import Optimizer
+        from pyai.optimization.optimizer.cutting_plane import Optimizer
         optimizer = Optimizer()
 
         #best_x = optimizer.optimize(f, num_iterations=7)   # sphere with 1 dimension
@@ -121,7 +127,7 @@ def main():
 
     elif optimizer_choice == "eda":
         # EDA #############################
-        #from optimizer.eda import Optimizer
+        #from pyai.optimization.optimizer.eda import Optimizer
         pass
 
     else:
