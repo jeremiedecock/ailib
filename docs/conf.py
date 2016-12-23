745841c1ec69d40af8fd425dfa917ad8cf0841b7
Jérémie Decock <jd.jdhp@gmail.com>
2016-12-23 19:46:41 +0100

    Fix a bug for Debian systems (rtd theme have to be imported and its path defined).

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 
 import sys
 import os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -144,6 +145,8 @@ html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
+if html_theme == 'sphinx_rtd_theme':
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
