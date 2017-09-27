8407810fbb35ec4939a496734bf823b3bf3cd8c4
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-09-27 19:38:47 +0200

    Add a generic plot function + misc improvements.

--- pyai/optimize/utils.py
+++ pyai/optimize/utils.py
@@ -37,7 +37,8 @@ Plots:
 - [ ] (benchmark session) err w.t. algorithms parameters (plot the iteration or evaluation number or execution time to reach in average an error lower than N% with e.g. N=99%)
 """
 
-__all__ = ['plot_contour_2d_solution_space',
+__all__ = ['array_list_to_array',
+           'plot_contour_2d_solution_space',
            'plot_2d_solution_space',
            'plot_fx_wt_iteration_number',
            'plot_err_wt_iteration_number',
@@ -49,82 +50,168 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from mpl_toolkits.mplot3d import axes3d
 
-def plot_fx_wt_iteration_number(fx_seq,
-                                ax=None):
-    """
-    Plot f(x*) w.t. iteration number.
+def array_list_to_array(y_array_list):
+    """Convert a sequence of 1D arrays (with possibly different sizes) to a 2D array.
 
-    TODO
+    When given 1D arrays don't have the same size, "missing" items at the end
+    of each 1D array are replaced by np.nan.
+
+    Parameters
+    ----------
+    y_array_list : sequence of ndarray
+        The sequence of 1D numpy arrays (with possibly different sizes) to
+        join.
+
+    Returns
+    -------
+    ndarray
+        The 2D numpy array made from the concatenation `y_array_list` items.
     """
+    max_size = max((y_array.shape[0] for y_array in y_array_list))
+    y_array = np.full((len(y_array_list), max_size), np.nan)
+
+    for index, y_array_i in enumerate(y_array_list):
+        y_array[index,:y_array_i.shape[0]] = y_array_i
+
+    return y_array
+
+
+def plot_1d(y,
+            x=None,
+            ax=None,
+            x_label=None,
+            y_label=None,
+            legend=None,
+            title=None,
+            x_log=False,
+            y_log=False,
+            plot_option=None,
+            **kwargs):
+    """Make a 1D plot.
+
+    Parameters
+    ----------
+    y : ndarray
+        The 1D or 2D numpy array containing the data's y-coordinate (ordinate)
+        to plot. If `y` is a 2D array, it is aggregated *on the first dimension*
+        (e.g. `y = np.nanmean(y, axis=0)`) which means the `y` array should be
+        defined like the following:
+        `y = np.array([sample_1, sample_2, ..., sample_n])`
+        where `sample_i` is a 1D numpy array containing for instance the error
+        of (the ith execution of) a minimizer w.t. the evaluation number
+        (i.e. `sample_1` is the errors of a first execution of the algorithm,
+        `sample_2` is the errors of a second execution of the algorithm,
+        ...).
+    x : ndarray
+        The 1D numpy array containing the data's x-coordinate (abscissa) to
+        plot.
+    ax : AxesSubplot
+        The Matplotlib axis object to plot on. If `ax` is `None`, a new axis is
+        created and automatically shown. Otherwise, the `ax` object is updated
+        with the plot but is not automatically shown.
+    x_label : string
+        The label for the x-axis.
+    y_label : string
+        The label for the y-axis.
+    legend : string
+        The legend label.
+    title : string
+        The plot title.
+    x_log : bool
+        If `True`, a log scale is used on the x-axis.
+    y_log : bool
+        If `True`, a log scale is used on the y-axis.
+    plot_option : string
+        Define the aggregation method if `y` is a 2D array. Possible values are
+        "mean" or "median".
+    **kwargs
+        Any arbitrary keyword arguments accepted by `matplotlib.pyplot.plot()`.
+    """
+    if y.ndim == 2:
+        # Aggregate data
+        if plot_option == 'mean':
+            y = np.nanmean(y, axis=0)
+        elif plot_option == 'median':
+            y = np.nanmedian(y, axis=0)
+        else:
+            raise ValueError("Two dimension arrays require a `plot_option` to define the aggregation method.")
+    elif y.ndim != 1:
+        raise ValueError("Wrong number of dimensions: {}.".format(y.ndim))
+
     if ax is None:
         fig, ax = plt.subplots(figsize=(12, 8))
 
-    ax.semilogy(fx_seq)
-    ax.set_xlabel("Iteration number")
-    ax.set_ylabel("f(x)")
+    if x_log:
+        ax.set_xscale("log", nonposx='clip')   # TODO
 
-    if ax is None:
-        plt.show()
+    if y_log:
+        ax.set_yscale("log", nonposy='clip')   # TODO
 
-
-def plot_err_wt_iteration_number(error_seq,
-                                 ax=None):
-    """
-    Plot error w.t. iteration number.
-
-    TODO
-    """
-    if ax is None:
-        fig, ax = plt.subplots(figsize=(12, 8))
-
-    ax.semilogy(error_seq)
-    ax.set_xlabel("Iteration number")
-    ax.set_ylabel("Error")
-
-    if ax is None:
-        plt.show()
-
-
-def plot_err_wt_execution_time(error_seq,
-                               time_seq,
-                               ax=None):
-    """
-    Plot error w.t. execution time.
-
-    TODO
-    """
-    if ax is None:
-        fig, ax = plt.subplots(figsize=(12, 8))
-
-    ax.semilogy(time_seq, error_seq)
-    ax.set_xlabel("Time (sec)")
-    ax.set_ylabel("Error")
-
-    if ax is None:
-        plt.show()
-
-
-def plot_err_wt_num_feval(error_seq,
-                          num_eval_seq=None,
-                          ax=None):
-    """
-    Plot error w.t. number of function evaluations.
-
-    TODO
-    """
-    if ax is None:
-        fig, ax = plt.subplots(figsize=(12, 8))
-
-    if num_eval_seq is not None:
-        ax.loglog(num_eval_seq, error_seq)
+    if x is None:
+        ax.plot(y, **kwargs)
     else:
-        ax.loglog(error_seq)
+        ax.plot(x, y, **kwargs)
 
-    ax.set_xlabel("Num fonction evaluations")
-    ax.set_ylabel("Error")
+    if legend is not None:
+        ax.legend(loc='best')
+
+    if x_label is not None:
+        ax.set_xlabel(x_label)
+
+    if y_label is not None:
+        ax.set_ylabel(y_label)
+
+    if title is not None:
+        ax.set_title(title)
 
     if ax is None:
         plt.show()
+
+
+def plot_fx_wt_iteration_number(fx_array, **kwargs):
+    """Plot f(x*) w.t. iteration number.
+
+    TODO
+    """
+    plot_1d(y=fx_array,
+            x_label="Iteration number",
+            y_label="f(x)",
+            **kwargs)
+
+
+def plot_err_wt_iteration_number(error_array, **kwargs):
+    """Plot error w.t. iteration number.
+
+    TODO
+    """
+    plot_1d(y=error_array,
+            x_label="Iteration number",
+            y_label="Error",
+            **kwargs)
+
+
+def plot_err_wt_execution_time(error_array, time_array, **kwargs):
+    """Plot error w.t. execution time.
+
+    TODO
+    """
+    plot_1d(y=error_array,
+            x=time_array,
+            x_label="Time (sec)",
+            y_label="Error",
+            **kwargs)
+
+
+def plot_err_wt_num_feval(error_array, num_eval_array=None, **kwargs):
+    """Plot error w.t. number of function evaluations.
+
+    TODO
+    """
+    plot_1d(y=error_array,
+            x=num_eval_array,
+            x_label="Num fonction evaluations",
+            y_label="Error",
+            **kwargs)
 
 
 def plot_contour_2d_solution_space(func,
@@ -136,7 +223,8 @@ def plot_contour_2d_solution_space(func,
                                    xstar=None,
                                    xvisited=None,
                                    title=""):
-    """
+    """Plot points visited during the execution of an optimization algorithm.
+
     TODO
     """
     if (fig is None) or (ax is None):                # TODO
@@ -219,7 +307,8 @@ def plot_2d_solution_space(func,
                            xvisited=None,
                            angle_view=None,
                            title=""):
-    """
+    """Plot points visited during the execution of an optimization algorithm.
+
     TODO
     """
     if fig is None or ax is None:                # TODO
