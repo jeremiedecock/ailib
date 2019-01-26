cf948ab96f340dc608a39b484f97d08a5e57455d
Jérémie Decock <jd.jdhp@gmail.com>
2019-01-26 14:36:48 +0100

    Update the version number + misc updates.

--- ailib/__init__.py
+++ ailib/__init__.py
@@ -1,46 +1,51 @@
-# PyAI
+"""AILib
 
-# The MIT License
-#
-# Copyright (c) 2014,2015,2016,2017 Jeremie DECOCK (http://www.jdhp.org)
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+AILib is a set of open source frameworks for Artificial Intelligence
+(mostly machine learning and optimization).
+
+This contains (among others):
+
+-  a blackbox non linear noisy optimization framework;
+-  a machine learning framework;
+-  a multistage optimization and Markov Decision Process framework
+   (Markov Decision Processes).
+
+Note:
+
+    This project is in beta stage.
+
+Viewing documentation using IPython
+-----------------------------------
+To see which functions are available in `ailib`, type ``ailib.<TAB>`` (where
+``<TAB>`` refers to the TAB key), or use ``ailib.*optimize*?<ENTER>`` (where
+``<ENTER>`` refers to the ENTER key) to narrow down the list.  To view the
+docstring for a function, use ``ailib.optimize?<ENTER>`` (to view the
+docstring) and ``ailib.optimize??<ENTER>`` (to view the source code).
+"""
 
 # PEP0440 compatible formatted version, see:
 # https://www.python.org/dev/peps/pep-0440/
 #
 # Generic release markers:
 # X.Y
-# X.Y.Z # For bugfix releases  
-# 
+# X.Y.Z # For bugfix releases
+#
 # Admissible pre-release markers:
 # X.YaN # Alpha release
-# X.YbN # Beta release         
-# X.YrcN # Release Candidate   
+# X.YbN # Beta release
+# X.YrcN # Release Candidate
 # X.Y # Final release
 #
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
-__version__ = '0.2.dev2'
+__version__ = '0.3.dev1'
 
-__all__ = ['ml',
-           'mdp',
-           'optimize',
-           'signal']
+def get_version():
+    return __version__
+
+# The following lines are temporary commented to avoid BUG#2 (c.f. BUGS.md)
+#from . import ml
+#from . import mdp
+#from . import optimize
+#from . import signal
