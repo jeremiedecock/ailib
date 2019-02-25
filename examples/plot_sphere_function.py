6a7cfc1c54074abf92f88e887dc8f85725cfe05a
Jeremie DECOCK <jd.jdhp@gmail.com>
2019-02-25 22:48:41 +0100

    Misc update.

--- examples/plot_sphere_function.py
+++ examples/plot_sphere_function.py
@@ -16,7 +16,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from ailib.utils.plot import plot_2d_contour_solution_space, plot_2d_solution_space
-from ailib.optimize.functions.unconstrained import sphere
+from ailib.optimize.functions.unconstrained import sphere2d as sphere
 
 ###############################################################################
 # Plot the sphere function
