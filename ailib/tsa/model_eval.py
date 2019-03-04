7007df8f67eaa42422625aca8e7a8e95ceb42d21
Jeremie DECOCK <jd.jdhp@gmail.com>
2019-03-04 01:39:03 +0100

    Misc improvements.

--- ailib/tsa/model_eval.py
+++ ailib/tsa/model_eval.py
@@ -29,6 +29,9 @@ __all__ = ['rolling_forecast']
 
 import numpy as np
 import pandas as pd
+
+import datetime
+import json
 import sys
 
 def rolling_forecast(data,
@@ -41,7 +44,7 @@ def rolling_forecast(data,
                      one_eval_per_period=False,
                      error_function=None,
                      error_label="Error",
-                     output_file_path="sarimax_results.json"):
+                     output_file_path="rolling_forecast_results.json"):
     """TODO
     
     Parameters
@@ -55,23 +58,29 @@ def rolling_forecast(data,
         TODO
     """
 
-    df = data
+    df = data.copy()
+        
+    df["error"] = np.nan
+    df["time"] = np.nan
     
-    forcast_list = [[] for l in range(periods_length)]
-    error_list = []
+    for i in range(periods_length):
+        df["predictions_" + str(i)] = np.nan
 
-    step = periods_length if one_eval_per_period else 1
+    exception_msg_dict = {}
+
+    step = periods_length * num_periods_predicted if one_eval_per_period else 1
 
     for start_learn_index in range(0,
-                                   len(df) - num_periods_learned - (num_periods_predicted * periods_length),
+                                   len(df) - (num_periods_learned * periods_length) - (num_periods_predicted * periods_length),
                                    step):
-        
+
+        dt_start = time.time()
+
         end_learn_index = start_learn_index + num_periods_learned * periods_length - 1
-        
         start_predict_index = end_learn_index+1
         end_predict_index = start_predict_index + num_periods_predicted * periods_length - 1
-        
-        print("Learn from index {} to index {} and predict from index {} to index {}".format(start_learn_index, end_learn_index, start_predict_index, end_predict_index))
+
+        print(start_learn_index, end_learn_index, start_predict_index, end_predict_index)
 
         try:
             model_fit = model.fit()
@@ -79,30 +88,34 @@ def rolling_forecast(data,
             model_forecast = model_fit.forecast(periods_length * num_periods_predicted,
                                                 exog=df.loc[start_predict_index:end_predict_index, exog].values.reshape(-1, 1))
 
-            forcast_list[start_learn_index % periods_length] += model_forecast.values.tolist()
+            column_str = "predictions_" + str(start_learn_index % periods_length)
+            df.loc[start_predict_index:end_predict_index, column_str] = model_forecast.values
 
             if error_function is not None:
                 error = error_function(df.loc[start_predict_index:end_predict_index, endog], model_forecast)
-                error_list.append(error)
                 print('{} = {:0.3f}'.format(error_label, error))
-            else:
-                error_list.append(float("nan"))
+
+                df.loc[start_predict_index, "error"] = error
 
         except Exception as e:
 
-            forcast_list[start_learn_index % periods_length] += [float("nan") for i in range(periods_length * num_periods_predicted)]
-            error_list.append(float("nan"))
-            print(e, file=sys.stderr)
+            key = "{}_{}_{}_{}".format(start_learn_index, end_learn_index, start_predict_index, end_predict_index)
+            exception_msg_dict[key] = str(e)
+            print(e)
 
-        # Save results in a JSON file #####################
-        
-        data = {
-            "predictions": forcast_list,
-            "error": error_list,
-            "time": datetime.datetime.now().isoformat()
-        }
+        finally:
 
-        with open(output_file_path, "w") as fd:
-            json.dump(data, fd, sort_keys=True, indent=4)  # pretty print format
+            dt_end = time.time()
+            df.loc[start_predict_index, "time"] = dt_end - dt_start
+            
+            # Save results in CSV
+            
+            df.to_csv(output_file_path + ".csv")
 
-    return forcast_list, error_list
+            # Save exception messages in a JSON file
+
+            with open(output_file_path + ".json", "w") as fd:
+                #json.dump(result_dict, fd)                           # no pretty print
+                json.dump(exception_msg_dict, fd, sort_keys=True, indent=4)  # pretty print format
+    
+    return df, exception_msg_dict
