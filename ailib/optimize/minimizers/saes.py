1ee57d670322416295c31f2457e14ff03f03508a
Jeremie DECOCK <jd.jdhp@gmail.com>
2019-03-20 00:01:20 +0100

    Pandas implementation.

--- ailib/optimize/minimizers/saes.py
+++ ailib/optimize/minimizers/saes.py
@@ -24,6 +24,15 @@ __all__ = ['SAES']
 
 import math
 import numpy as np
+import pandas as pd
+
+try:
+    import matplotlib
+    import matplotlib.pyplot as plt
+    from matplotlib import cm
+    from matplotlib import colors
+except Exception as e:
+    print(e)
 
 from .optimizer import Optimizer
 
@@ -32,6 +41,26 @@ class SAES(Optimizer):
     """SAES optimizer.
 
     ($\mu$/1+$\lambda$)-$\sigma$-Self-Adaptation-ES
+
+
+    Init pop
+
+    $\forall$ gen
+
+    $\quad$ $\forall$ child
+
+    $\quad\quad$ 1. select $\rho$ parents
+
+    $\quad\quad$ 2. recombination of selected parents (if $\rho > 1$)
+
+    $\quad\quad$ 3. mutation of $\sigma$ (individual strategy) : $\sigma \leftarrow \sigma ~ e^{\tau \mathcal{N}(0,1)}$
+
+    $\quad\quad$ 4. mutation of $\boldsymbol{x}$ (objective param) : $\boldsymbol{x} \leftarrow \boldsymbol{x} + \sigma ~ \mathcal{N}(0,1)$
+
+    $\quad\quad$ 5. eval $f(\boldsymbol{x})$
+    
+    $\quad$ Select next gen individuals
+
 
     See:
     * http://www.scholarpedia.org/article/Evolution_strategies
@@ -62,7 +91,9 @@ class SAES(Optimizer):
                  lmb=6,
                  rho=1,
                  tau=None,
-                 selection_operator='+'):
+                 selection_operator='+',
+                 isotropic_mutation=True,
+                 plot=False):
         """TODO
 
         Parameters
@@ -76,10 +107,27 @@ class SAES(Optimizer):
             The optimal point found (a 1D numpy array).
         """
 
+        assert selection_operator in (',', '+')
+
+        # Number of dimension of the solution space
         d = objective_function.ndim
 
+        # Self-adaptation learning rate
         if tau is None:
-            tau = 1./math.sqrt(2.*d)         # self-adaptation learning rate
+            tau = 1./math.sqrt(2.*d)
+
+        # Set indices alias ############################
+
+        all_indices = slice(None, None)
+        parent_indices = slice(0, mu)
+        children_indices = slice(mu, None)
+
+        sigma_col = 0
+        x_cols = slice(1, -1)
+        y_col = -1
+
+        sigma_col_label = "sigma"
+        y_col_label = "y"
 
         # Init the population ##########################
 
@@ -90,71 +138,120 @@ class SAES(Optimizer):
         # - the last column contains the individual's assess (f(x))
         # - the other columns contain the individual value (x)
 
-        pop = np.full([mu+lmb, d+2], np.nan)
-        pop[:mu, 0] = 1.                                       # init the parents strategy to 1.0
-        pop[:mu, 1:-1] = np.random.normal(init_pop_mean,
-                                          init_pop_std,
-                                          size=[mu,d])         # init the parents value
-        pop[:mu, -1] = objective_function(pop[:mu, 1:-1].T)    # evaluate parents
+        pop = pd.DataFrame(np.full([mu+lmb, d+2], np.nan),
+                           columns=[sigma_col_label] + ["x" + str(d) for d in range(d)] + [y_col_label])
+
+        pop.iloc[parent_indices, sigma_col] = 1.                                    # init the parents strategy to 1.0
+        #pop.iloc[parent_indices, x_cols] = np.random.normal(init_pop_mean,
+        #                                                    init_pop_std,
+        #                                                    size=[mu, d])          # init the parents value
+        pop.iloc[parent_indices, x_cols] = np.random.uniform(low=-10., high=10., size=[mu, d])    # init the parents value
+        pop.iloc[parent_indices, y_col] = objective_function(pop.iloc[parent_indices, x_cols].values.T)  # evaluate parents
         #print("Initial population:\n", pop, "\n")
 
-        ## Sort parents
-        #pop = pop[pop[:,-1].argsort()]
-        #print(pop)
+        # Plot #############################################
+
+        if plot:
+            cmap = cm.gnuplot2 # magma
+
+            fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=(20, 6))
+            ax3.set_xlabel('gen')
+            ax3.set_ylabel('y')
 
         for gen in range(num_gen):
 
-            # Make children ################################
-
+            # Parent selection #############################
+    
             if rho == 1:
+                
                 # Each child is made from one randomly selected parent
-                pop[mu:,:] = pop[np.random.randint(mu, size=lmb)]
+                selected_parent_indices = np.random.randint(mu, size=lmb)
+            
             elif rho == mu:
+            
                 # Recombine all parents for each child
                 raise NotImplemented() # TODO
+            
             elif 1 < rho < mu:
+            
                 # Recombine rho randomly selected parents for each child
                 raise NotImplemented() # TODO
+            
             else:
+            
                 raise ValueError()
+                
+            #print("Parent selection")
+            #display(selected_parent_indices)
+            
+            # Recombination ################################
 
-            pop[mu:,-1] = np.nan
-            #print("Children:\n", pop, "\n")
+            if rho == 1:
+                
+                # Each child is made from one randomly selected parent
+                pop.iloc[children_indices] = pop.iloc[selected_parent_indices].values
+                
+            elif rho == mu:
+                
+                # Recombine all parents for each child
+                raise NotImplemented() # TODO
+                
+            elif 1 < rho < mu:
+                
+                # Recombine rho randomly selected parents for each child
+                raise NotImplemented() # TODO
+                
+            else:
+                
+                raise ValueError()
+            
+            pop.iloc[children_indices, y_col] = np.nan
+            
+            #print("Recombination")
+            #display(pop)
 
             # Mutate children's sigma ######################
-
-            pop[mu:,0] = pop[mu:,0] * np.exp(tau * np.random.normal(size=lmb))
-            #print("Mutated children (sigma):\n", pop, "\n")
+    
+            pop.iloc[children_indices, sigma_col] *= np.exp(tau * np.random.normal(size=lmb))
+            
+            #print("Mutated children's sigma")
+            #display(pop)
 
             # Mutate children's value ######################
-
-            sigma_array = np.tile(pop[mu:,0],[d,1]).T
-            #print("Sigma array:\n", sigma_array, "\n")
-
+    
+            sigma_array = np.tile(pop.iloc[children_indices, sigma_col], [d,1]).T    # TODO: <- CHECK THIS !!!
             random_array = np.random.normal(size=[lmb,d])
-            #print("Random array:\n", random_array, "\n")
+            pop.iloc[children_indices, x_cols] += sigma_array * random_array
 
-            pop[mu:,1:-1] = pop[mu:,1:-1] + sigma_array * random_array
-            #print("Mutated children (value):\n", pop, "\n")
+            #print("Mutated children's value")
+            #display(pop)
 
             # Evaluate children ############################
 
-            pop[mu:, -1] = objective_function(pop[mu:, 1:-1].T)
-            #print("Evaluated children:\n", pop, "\n")
+            pop.iloc[children_indices, y_col] = objective_function(pop.iloc[children_indices, x_cols].values.T)
+
+            #print("Evaluate children")
+            #display(pop)
+
+            if plot:
+                color_str = matplotlib.colors.rgb2hex(cmap(float(gen) / num_gen))
+                pop.plot.scatter(x="x0", y="x1", c=color_str, ax=ax1)
+                pop.plot.scatter(x="sigma", y="y", c=color_str, loglog=True, ax=ax2)
+                ax3.semilogy(np.full(shape=pop.y.shape, fill_value=gen), pop.y, '.', color=color_str)
 
             # Select the best individuals ##################
 
-            if selection_operator == '+':
-                # *plus-selection* operator
-                pop = pop[pop[:,-1].argsort()]
-            elif selection_operator == ',':
-                # *comma-selection* operator
-                pop[:lmb,:] = pop[pop[mu:,-1].argsort()]   # TODO: check this...
-            else:
-                raise ValueError()
+            if selection_operator == ',':
+                pop.iloc[parent_indices] = np.nan
+            
+            pop = pop.sort_values(by=[y_col_label], na_position='last').reset_index(drop=True)
+            
+            pop.iloc[children_indices] = np.nan
+            
+            #print("Selected individuals for the next generation")
+            #display(pop)
 
-            pop[mu:, :] = np.nan
+        if plot:
+            plt.show()
 
-            #print("Selected individuals for the next generation:\n", pop, "\n")
-
-        return pop[0, 1:-1]
+        return pop.iloc[0, x_cols].values
